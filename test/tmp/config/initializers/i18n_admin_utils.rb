f66aa34fb8266f1efdb1c89b9e8a3e72e63ba9ae
Timothee Guerin <timothee.guerin@outlook.com>
2014-04-10 18:27:22 -0400

    Removed js prints

--- test/tmp/config/initializers/i18n_admin_utils.rb
+++ test/tmp/config/initializers/i18n_admin_utils.rb
@@ -1,26 +1,28 @@
-I18nAdminUtils.config do |config|
+if Rails.env.development?
+  I18nAdminUtils.config do |config|
 
-  #Specify the backend if you are using the chain(it will use the first one by default)
-  #config.backend = 'I18n::Backend::Simple'
-  #config.backend = 'I18n::Backend::ActiveRecord'
+    #Specify the backend if you are using the chain(it will use the first one by default)
+    #config.backend = 'I18n::Backend::Simple'
+    #config.backend = 'I18n::Backend::ActiveRecord'
 
-  #Model that contains the translation must have locale, key and value columns
-  config.translation_model = Translation
+    #Model that contains the translation must have locale, key and value columns
+    config.translation_model = Translation
 
-  #Reload the translation after they have been updated
-  config.reload_translation_after_update = true
+    #Reload the translation after they have been updated
+    config.reload_translation_after_update = true
 
-  #List of locales used in your application
-  config.locales = ['en']
+    #List of locales used in your application
+    config.locales = ['en']
 
 
-  #List of folder where the app will look for the use of the t() method
-  config.search_folders = ["#{Rails.root}/app"]
+    #List of folder where the app will look for the use of the t() method
+    config.search_folders = ["#{Rails.root}/app"]
 
-  #Yml files if you are using a yml backend
-  config.yml_file = "#{Rails.root}/config/locales/i18_admin_utils"
+    #Yml files if you are using a yml backend
+    config.yml_file = "#{Rails.root}/config/locales/i18_admin_utils"
 
-  #To exclude some folder included in the search_folders
-  #Not working yet!
-  #config.search_folders_exclude = []
+    #To exclude some folder included in the search_folders
+    #Not working yet!
+    #config.search_folders_exclude = []
+  end
 end
