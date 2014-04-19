9c5ca8cb3b5f03ae7f99809874b683bb47409269
Timothee Guerin <timothee.guerin@outlook.com>
2014-04-19 18:38:58 -0400

    added title tootip with missing translation list

--- app/helpers/i18n_admin_utils/application_helper.rb
+++ app/helpers/i18n_admin_utils/application_helper.rb
@@ -6,9 +6,11 @@ module I18nAdminUtils
       missing_translations = translation.missing_translations
       color_id = (missing_translations.size.to_f/translation.translations.size.to_f*5).ceil-1
       if missing_translations.size == 0
-        content_tag('span', '', :class => 'glyphicon glyphicon-ok greentext')
+        content_tag 'span', '', :class => 'glyphicon glyphicon-ok greentext',
+                    :title => 'This key has been translated in all languages', :rel => 'tooltip'
       else
-        content_tag('span', "(#{missing_translations.size})", :class => "color-range-#{color_id} bold")
+        content_tag 'span', "(#{missing_translations.size})", :class => "color-range-#{color_id} bold",
+                    :title => missing_translations.keys.join(','), :rel => 'tooltip'
       end
 
     end
